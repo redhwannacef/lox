8e4b12f35241e5b8a766636db08aa5bc867fe3ae
Redhwan Nacef <nacefredhwan@gmail.com>
2026-03-02 16:23:44 +0000

    Fix scope issues with Resolver
    Issues being local variables can override closures. We add a resolved to
    allow static analysis of this and other static issues like trying to
    return outwith a function.

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,7 +1,9 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.craftinginterpreters.lox.Expr.Assign;
 import com.craftinginterpreters.lox.Expr.Binary;
@@ -18,6 +20,7 @@ import com.craftinginterpreters.lox.Stmt.Var;
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   final Environment globals = new Environment();
   private Environment environment = globals;
+  private final Map<Expr, Integer> locals = new HashMap<>();
 
   Interpreter() {
     globals.define("clock", new LoxCallable() {
@@ -144,13 +147,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   @Override
   public Object visitVariableExpr(Variable expr) {
-    return environment.get(expr.name);
+    return lookUpVariable(expr.name, expr);
   }
 
   @Override
   public Object visitAssignExpr(Assign expr) {
     Object value = evaluate(expr.value);
-    environment.assign(expr.name, value);
+
+    Integer distance = locals.get(expr);
+    if (distance != null)
+      environment.assignAt(distance, expr.name, value);
+    else
+      globals.assign(expr.name, value);
+
     return value;
   }
 
@@ -292,6 +301,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     } finally {
       this.environment = previous;
     }
+  }
+
+  void resolve(Expr expr, int depth) {
+    locals.put(expr, depth);
+  }
+
+  private Object lookUpVariable(Token name, Expr expr) {
+    Integer distance = locals.get(expr);
+    if (distance != null)
+      return environment.getAt(distance, name.lexeme);
+    else
+      return globals.get(name);
+
   }
 
 }
