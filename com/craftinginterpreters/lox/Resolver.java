87bcec05bdb6db72dd46d246e55d8188eebb7163
Redhwan Nacef <nacefredhwan@gmail.com>
2026-03-04 16:03:46 +0000

    Add constructor to classes

--- com/craftinginterpreters/lox/Resolver.java
+++ com/craftinginterpreters/lox/Resolver.java
@@ -85,8 +85,13 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     if (currentFunction == FunctionType.NONE)
       Lox.error(stmt.keyword, "Can't return from top-level code.");
 
-    if (stmt.value != null)
+    if (stmt.value != null) {
+      if (currentFunction == FunctionType.INITIALIZER)
+        Lox.error(stmt.keyword, "Can't return a value from an initializer.");
+
       resolve(stmt.value);
+    }
+
     return null;
   }
 
@@ -174,6 +179,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     for (Stmt.Function method : stmt.methods) {
       FunctionType declaration = FunctionType.METHOD;
+      if (method.name.lexeme.equals("init"))
+        declaration = FunctionType.INITIALIZER;
       resolveFunction(method, declaration);
     }
 
@@ -270,6 +277,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private enum FunctionType {
     NONE,
     FUNCTION,
+    INITIALIZER,
     METHOD
   }
 
